a9cad2ed0af0484a7febab2af60fd63bf03cb3e1
senyoltw <senyoltw@users.noreply.github.com>
2020-04-07 03:21:37 +0900

    ver 1.0.0

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # heos-amp-sync
-Sync Amplifier and Network Player using HEOS and NATURE REMO
+Sync Amplifier and Network Audio Player using HEOS and NATURE REMO
+
+# How to USE
+1. Get Nature Remo token
+2. Get amp on/off signal
+```
+% curl -H 'Authorization: Bearer <token>' -H "accept: application/json" -X GET "https://api.nature.global/1/appliances" |jq
+```
+3. write to eos-amp-sync.js
+```
+/// set your token
+let remo_token = 'set your token'
+/// set your signal
+let remo_signal = 'set your signal'
+```
+4. npm install and npm start
---
+++ heos-amp-sync.js
@@ -0,0 +1,53 @@
+const heos = require('heos-api')
+const request = require('request')
+
+/// set your token
+let remo_token = 'set your token'
+/// set your signal
+let remo_signal = 'set your signal'
+
+let cur_state = 'null'
+const options = {
+    url: `https://api.nature.global/1/signals/${remo_signal}/send`,
+    headers: { 'authorization': `Bearer ${remo_token}` },
+    method: 'POST'
+}
+
+function amp_power(state) {
+    if (state == cur_state) {
+        // console.log('amp.state.same do nothing')
+    } else {
+        console.log(`amp.state.change ${cur_state} => ${state} send IR`)
+        request(options, (error, response, body) => {
+            if (error) {
+                console.log(`failed to update: ${error}, ${body}`)
+            } else {
+                cur_state = state
+            }
+        }
+        )
+    }
+}
+
+heos.discoverAndConnect().then(connection =>
+    connection
+        //.onAll(console.log)
+        .write('system', 'register_for_change_events', { enable: 'on' })
+        .write('system', 'prettify_json_response', { enable: 'on' })
+
+        .on(
+            {
+                commandGroup: 'event',
+                command: 'player_state_changed'
+            },
+            data => {
+                if (data.heos.message.parsed.state == 'stop') {
+                    // console.log('amp.off')
+                    amp_power('off')
+                } else {
+                    // console.log('amp.on')
+                    amp_power('on')
+                }
+            }
+        )
+)
